804a2e16842c255c751ce9ea953c78319bfc6674
wch940770 <65217071+wch940770@users.noreply.github.com>
2024-04-15 19:37:23 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-05a03e6058638183d"
   instance_type = "t2.micro"
-  subnet_id = "subnet-0bc6ed2bac4e36a13"
+  subnet_id = "subnet-02544383bc5c0a096"
   security_groups = ["sg-07b1c21dbad6152da"]
   tags = {
     "Name" = "ralftest-2"
