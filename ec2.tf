44f011881feaedbf7378112e62a9eb84c2496855
wch940770 <65217071+wch940770@users.noreply.github.com>
2024-04-15 16:27:14 +0800

    Update ec2.tf
    subnet_id = ran-test-2

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-05a03e6058638183d"
   instance_type = "t2.micro"
+  subnet_id = "subnet-0bc6ed2bac4e36a13"
   tags = {
     "Name" = "ralftest-2"
   }
